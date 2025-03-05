87ab66d639ec70581267d6b5f6dd23f00012cd27
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2025-03-05 19:17:36 +0000

    MainLayout: Concluído

--- client_code/Layouts/MainLayout/__init__.py
+++ client_code/Layouts/MainLayout/__init__.py
@@ -1,10 +1,14 @@
 from ._anvil_designer import MainLayoutTemplate
 from anvil import *
+from anvil.js import get_dom_node
 
 class MainLayout(MainLayoutTemplate):
     def __init__(self, **properties):
         # Changing styles
         self.main_card.dom_nodes['anvil-m3-card'].style.minHeight = '89vh'
+        self.header_anchor.dom_nodes['anvil-m3-link'].classList.add('header-app-image')
+        get_dom_node(self.header_panel).classList.add('hidden-when-mobile')
+        get_dom_node(self.user_icon_component_1).classList.add('hidden-when-fullsize')
         # self.header_panel.visible = False
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
