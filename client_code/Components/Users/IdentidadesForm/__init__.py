7a7639994949a82d0fb6d7131c30ead3ac006ae3
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2025-03-06 04:57:33 +0000

    Edited Components.Users.IdentidadesForm

--- client_code/Components/Users/IdentidadesForm/__init__.py
+++ client_code/Components/Users/IdentidadesForm/__init__.py
@@ -8,3 +8,15 @@ class IdentidadesForm(IdentidadesFormTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+        self.layout.set_required_components([
+            self.layout.fullname_text_box,
+            self.layout.display_name_text_box,
+            self.cpf_text_box
+        ], 'identidadeValidationGroup')
+        
+        if self.layout.mode == 'INIT':
+            self.dados_profissionais_card.visible = False
+        self.layout.set_steps_list([
+            {'card': self.layout.identidades_card, 'validation': 'identidadeValidationGroup'},
+            {'card': self.dados_profissionais_card}
+        ])
