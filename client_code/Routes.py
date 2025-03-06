065daa88c72bb88f69a8aea65c12f9e886a6ce36
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2025-03-06 20:27:49 +0000

    Rotas seguras verificam se o usuário foi concluído com sucesso.

--- client_code/Routes.py
+++ client_code/Routes.py
@@ -1,6 +1,6 @@
 from anvil import app
 
-from OruData.Routing import launch, Route, SecureRoute, CrudRoute, UnloggedRoute, Redirect
+from OruData.Routing import launch, Route, CrudRoute, UnloggedRoute, Redirect, SecureRoute as BaseSecureRoute
 # from .Entities import 
 
 IN_DEBUG_MODE = "debug" in app.environment.tags
@@ -14,9 +14,20 @@ Route.create(path='/', form='Pages.Dashboard')
 Route.create(path="/about", form="Pages.Outros.About")
 Route.create(path='/changelog', form="Pages.Outros.Changelog")
 
+class SecureRoute(BaseSecureRoute):
+    """Todas as rotas seguras devem antes checar se o usuário logado já completou o cadastro também.
+    Se não está logado, redireciona ao login.
+    Se está logado e não completou o cadastro, redireciona ao init
+    """
+    def before_load(**loader_args):
+        user = super().before_load(**loader_args)
+        # Se chegou aqui, então está logado
+        if not (user['fullname'] and user['display_name'] and user['cpf']):
+            raise Redirect('/user/init')
+
 # Users
 class UserSetup(SecureRoute, Route):
-    path = "/user/setup"
+    path = "/user/init"
     form = "Components.Users.IdentidadesForm"
 
 class UserIdentity(SecureRoute, Route):
@@ -30,7 +41,7 @@ class LoginForm(UnloggedRoute, Route):
 class LoginRoute(LoginForm):
     path = "/login"
 
-class SignupRoute:
+class SignupRoute(LoginForm):
     path = "/signup"
 
 class NotFoundRoute(Route):
