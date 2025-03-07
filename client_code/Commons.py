8be2507052814dc77c722275944560823f39ed40
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2025-03-07 00:18:28 +0000

    Edited settings and Commons

--- client_code/Commons.py
+++ client_code/Commons.py
@@ -1,6 +1,6 @@
 from OruData.Globals import CommonData, singleton_class, call_async
 
-def after_login_method(user, context, **event_args):
+def after_login_method(user, context=None, **event_args):
     if user is not None and not (user['fullname'] and user['display_name'] and user['cpf']):
         from OruData.Routing import navigate
         navigate(path='/user/init')
@@ -9,3 +9,6 @@ def after_login_method(user, context, **event_args):
 @singleton_class(merge_subclass=True)
 class LocalCommons(CommonData):
     pass
+    # def load_data(self):
+    #     CommonData.load_data(self)
+    #     after_login_method(self._logged_user)
