2b7c667e1f624c864be0c27eac23af9456e2e9ca
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2025-03-07 22:34:00 +0000

    Filtro de Pacientes concluido

--- client_code/Pages/Pacientes/__init__.py
+++ client_code/Pages/Pacientes/__init__.py
@@ -3,9 +3,11 @@ from anvil import *
 from anvil.js import get_dom_node
 
 from ...Commons import LocalCommons
+import re
 
 class Pacientes(PacientesTemplate):
     def __init__(self, **properties):
+        self.date_pattern = re.compile(r'\D')
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
@@ -25,3 +27,9 @@ class Pacientes(PacientesTemplate):
         pacientes = LocalCommons().get_pacientes()
         query = self.paciente_nome_filter_text_box.text
         self.pacientes_data_panel.items = [p for p in pacientes if not query or query in p['nome']]
+
+    def paciente_nascimento_filter_text_box_change(self, **event_args):
+        """This method is called when the text in this component is edited."""
+        pacientes = LocalCommons().get_pacientes()
+        query = self.date_pattern.sub('', self.paciente_nascimento_filter_text_box.text) if self.paciente_nascimento_filter_text_box.text else None
+        self.pacientes_data_panel.items = [p for p in pacientes if not query or query in p['nascimento'].strftime('%d%m%Y')]
