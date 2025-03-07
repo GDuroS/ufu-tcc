6dd5ee53a3ff7595dd285d2d7a3233a7e30f4679
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2025-03-07 00:50:40 +0000

    Edited Components.Users.IdentidadesForm

--- client_code/Components/Users/IdentidadesForm/__init__.py
+++ client_code/Components/Users/IdentidadesForm/__init__.py
@@ -1,10 +1,12 @@
 from ._anvil_designer import IdentidadesFormTemplate
 from anvil import *
 
+from ....Entities import ProfissionalUser
 
 class IdentidadesForm(IdentidadesFormTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        self.layout.user_class = ProfissionalUser
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
@@ -36,3 +38,4 @@ class IdentidadesForm(IdentidadesFormTemplate):
     @mode.setter
     def mode(self, mode):
         self.layout.mode = mode
+        
